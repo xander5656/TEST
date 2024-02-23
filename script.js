9d969a94fd87c90312daedf2b31ae363277a63dd
xander5656 <160562639+xander5656@users.noreply.github.com>
2024-02-22 23:50:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.yes-btn').addEventListener('click', function() {
+    alert('¡WAOSSSS! ¡Almenos lo reconoces❤!');
+  });
+  document.querySelector('.no-btn').addEventListener('click', function() {
+    alert('¡Ups! Parece que no puedo aceptar un "no" como respuesta. ¿Qué tal si pruebas con el boton de ¨´SI¨?');
+  });
